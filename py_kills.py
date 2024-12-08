ef5048b1018871fed82a78e4bff42b691e9b0faf
hu56-dot <wfl1913syj@163.com>
2024-12-08 18:33:53 +0800

    二三层转发学习

--- py_kills.py
+++ py_kills.py
@@ -1,3 +1,11 @@
-def test():
-    print("hello")
+a = [12,230,10,220,9,100]
 
+max_number = 0
+max_drop = 0
+
+for i in range(len(a)):
+    if a[i] > max_number:
+        max_number = a[i]
+    elif (max_number -a[i]) > max_drop:
+        max_drop = max_number - a[i]
+print("Max drop is", max_drop)
